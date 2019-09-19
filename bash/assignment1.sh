406ac74b5fbbabe29663fdb81492bd8088e0002d
akshaykotra <200412506@student.georgiancollege.ca>
2019-09-19 06:37:58 -0700

    assignment 1 10 largest files

--- bash/assignment1.sh
+++ bash/assignment1.sh
@@ -17,3 +17,4 @@ echo "============="
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 echo "10 largest files in the system:"
 echo "============="
+find / -type f -print0 | xargs -0 du -h | sort -rh | head -n 10
